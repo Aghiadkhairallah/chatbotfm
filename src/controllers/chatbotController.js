4a097e8d3300d7763ad2e6db646b06108291ff79
unknown <aghiad@techfugees.com>
2022-07-04 04:44:57 +0300

    update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -79,9 +79,10 @@ function handleMessage(sender_psid, received_message) {
   // Check if the message contains text
   if (received_message.text) {    
 
-    // Create the payload for a basic text message``
-    patterns=  ` ["Hi there", "How are you", "Is anyone there?","Hey","Hola", "Hello", "Good day", "Hey", "Ekse", "Hi"],`
-    responses= `["Hi stranger", "Yebo yes, how can I help", "Ey watsupp", "Ekse Hoezit", "Hola", "Hey dude", "You again"]`
+    // Create the payload for a basic text message
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+    }
   }  
   
   else if (received_message.attachments) {
